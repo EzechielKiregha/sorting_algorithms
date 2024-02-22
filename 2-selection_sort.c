47419e5c75e18bc3579491e9af15515024e3a17a
EzechielKiregha <ekiregha@gmail.com>
2024-02-22 02:32:11 +0200

    Selection algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,7 @@
 nclude "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
+ * let_them_swap - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
